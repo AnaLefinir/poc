a008454388226498e4ae881305e7c8be92532c23
Ana Lefinir <analefinir@gmail.com>
2016-03-04 23:29:43 -0300

    ultimos arreglos

--- express1/express_assay.js
+++ express1/express_assay.js
@@ -2,34 +2,41 @@
  * Created by Anita on 04/03/2016.
  */
 var express = require('express'),
-    fs = require('fs');
-    app = express(),
+    fs = require('fs'),
+    app = express();
 
-app.get('/puchun/:type', function(req, res){
+app.get('/puchun/:type', function (req, res) {
     var message;
     var type;
     var some;
 
-    some = req.params.some;
+    some = req.query.some;
     type = req.params.type;
 
-    if(type === 'hermosa'){
-        message = 'miau'
-    }else if(type === 'gorda'){
-        message = 'hiss!'
+    if (type === 'hermosa') {
+        message = 'miau';
+    } else if (type === 'gordo') {
+        message = 'hiss!';
+    } else {
+        res.writeHead(404, {
+            'X-STATUS': 'palurdo...',
+            'Content-Type': 'text/xml'
+        });
+        res.write('<puchun>pero que palurdo</puchun>');
+        res.end();
+        return;
     }
 
-    fs.readFile('./puchunresponse.json',function(err, data){
-        var result = JSON.parse(data);
-        result.message = message;
-        result.some = some;
-        result = JSON.stringify(data);
+    fs.readFile('./puchunresponse.json', function (err, data) {
+        var object = JSON.parse(data);
+        object.message = message;
+        object.some = some;
+        var serializedObject = JSON.stringify(object);
 
-        res.write(result);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(serializedObject);
         res.end();
     });
 
 });
-
-
 app.listen(8080);
